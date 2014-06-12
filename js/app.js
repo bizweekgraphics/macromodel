c5ac6d7c9bd0b9094c6ce385bde151433f43c56e
Toph Tucker <tophtucker@gmail.com>
2014-06-11 22:28:21 -0400

    Considering a generic Variable class.
    Oh also, as implemented, the capital/income ratio diverges. That's
    wrong! It should be converging to s/g! Gotta use a better saving &
    production model; cf. Krusell/Smith.

--- js/app.js
+++ js/app.js
@@ -7,15 +7,22 @@ $(function(){
 
   // assume g in [0.01, 0.015]
   var g = _.random(10,15)/1000;
+  console.log("g: " + g);
 
   // assume s in [0.1-0.12]
   var s = _.random(10,12)/100;
+  console.log("s: " + s);
+
+  console.log("s/g:" + s/g);
 
   // initial endowment of capital
   var k = _.random(1000);
+  console.log("k: "+k);
 
   // initial income; in U.S., currently about 4*capital
   var y = 4*k;
+  console.log("y: "+y);
+
 
   // capital / income ratio
   var β = k/y;
@@ -38,5 +45,19 @@ $(function(){
 
   render();
   setInterval(update,1000);
+
+
+
+  var Variable = Backbone.Model.extend({
+    defaults: {
+      "symbol": "x",
+      "name": "Variable",
+      "description": "A new variable",
+      "value": 0,
+      "formula": function() { return this.value; },
+      "formulaDisplay": ""
+    }
+
+  });
 
 });
