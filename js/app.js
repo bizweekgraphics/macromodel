aa4e10f840e1b082e237421b609ba06741bad9d0
Toph Tucker <tophtucker@gmail.com>
2014-06-13 00:31:32 -0400

    Building a system of equations as a Backbone collection?
    This is .... hard.

--- js/app.js
+++ js/app.js
@@ -1,30 +1,11 @@
 $(function(){
 
   var time = 0;
-
-  // assume r = 0.05;
   var r = 0.05;
-
-  // assume g in [0.01, 0.015]
   var g = _.random(10,15)/1000;
-  console.log("g: " + g);
-
-  // assume s in [0.1-0.12]
   var s = _.random(10,12)/100;
-  console.log("s: " + s);
-
-  console.log("s/g:" + s/g);
-
-  // initial endowment of capital
   var k = _.random(1000);
-  console.log("k: "+k);
-
-  // initial income; in U.S., currently about 4*capital
   var y = 4*k;
-  console.log("y: "+y);
-
-
-  // capital / income ratio
   var β = k/y;
 
   function render() {
@@ -34,30 +15,72 @@ $(function(){
   }
 
   function update() {
-
     k *= 1 + r*(1-s);
     y *= 1 + g;
     β = k/y;
-
     render();
-
   }
 
   render();
   setInterval(update,1000);
 
-
+  // Now let's overengineer it!
 
   var Variable = Backbone.Model.extend({
     defaults: {
-      "symbol": "x",
-      "name": "Variable",
-      "description": "A new variable",
-      "value": 0,
-      "formula": function() { return this.value; },
-      "formulaDisplay": ""
+      "name": "y",
+      "dependence": [],
+      "formula": function() { return false; }
+    },
+    initialize: {
+      this.dependence = getParamNames(this.get("formula"));
     }
-
   });
 
+  var system = new Backbone.Collection;
+  system.add([
+
+    new Variable({
+      name: "r",
+      formula: function() { return 0.05; }
+    }),
+
+    new Variable({
+      name: "g",
+      formula: function() { return _.random(10,15)/1000; }
+    }),
+
+    new Variable({
+      name: "s",
+      formula: function() { return _.random(10,12)/100; }
+    }),
+
+    new Variable({
+      name: "k",
+      formula: function() { return _.random(1000); }
+    }),
+
+    new Variable({
+      name: "y",
+      formula: function(k) { return 4*k; }
+    }),
+
+    new Variable({
+      name: "β",
+      formula: function(y,k) { return y/k; }
+    })
+    
+  ]);
+
 });
+
+// from http://stackoverflow.com/a/9924463/120290
+var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+var ARGUMENT_NAMES = /([^\s,]+)/g;
+function getParamNames(func) {
+  var fnStr = func.toString().replace(STRIP_COMMENTS, '')
+  var result = fnStr.slice(fnStr.indexOf('(')+1, fnStr.indexOf(')')).match(ARGUMENT_NAMES)
+  if(result === null)
+     result = []
+  return result
+}
